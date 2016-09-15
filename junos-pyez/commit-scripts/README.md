375a3cbb789ed659157dc729f388a1afa0aa4cc7
nate <nathan.sellers@badcock.com>
2016-09-15 13:16:01 -0400

    Initial commit

---
+++ junos-pyez/commit-scripts/README.md
@@ -0,0 +1,62 @@
+# Junos Configuration Change
+This script applies configuration changes to Junos devices. 
+
+## Overview
+
+This script uses `set` commands to apply configuration changes to the Junos device. There are other methods of applying configuration changes such as XML, .conf files, YAML and Jinja2 Templates but for simplicity this particular script only uses Junos `set` commands.
+
+The "_junos-config-change.py_" file has `#comments` throughout the entire script that briefly explain the purpose of the various commands. 
+
+Below is an example of the standard structure that every script follows.
+
+## Steps
+The script loops over a list of IPs and follows these steps to apply configuration changes:
+
++ Opens a connection to the SRX using the provided credentials
++ Locks the configuration (`configure exclusive`)
++ Checks for uncommitted changes (`show | compare`)
+    - If uncommitted changes are found it clears them (`rollback 0`)
+    - After clearing it checks again to ensure there are no pending changes
++ Loads the configuration changes
++ Tests the configuration for obvious syntax errors (`commit check`) 
++ Commits the configuration with 1 minute to confirm (`commit confirm 1`)
++ Tests the connectivity to the SRX
+    - If it has connectivity (the changes didn't break anything) it confirms the commit (`commit`)
+    - If it has no connectivity it exits the device and the SRX will rollback in 1 minute
++ Unlocks the configuration 
++ Closes connection to the SRX
+
+## Results
+The results of the script are printed on the console screen and are also logged to a file. The results file will be named according to what you entered in the `location` variable. The 'Results' folder contains subfolders that hold the results of a particular script and the IR that it relates to.
+```python
+f = open('junos-config-change-results-%s.txt' % location, 'w') 
+```
+An error file is also generated at the start of every script run. When a script finishes running it checks the error file to see if it is empty. If the error file is empty the script automatically deletes it. 
+
+The code responsible for creating the error file:
+```python
+fail_log = 'junos-config-change-error-report-%s.txt' % location
+```
+An example of the script output can be viewed by clicking on the "_junos-config-change-results.txt_" file
+
+## Precautions
+**This script uses several different safety precautions and has exception handling at every step.**
+
+
+* The biggest concern is making a configuration change that brings down the SRX or causes it to lose connection. To safeguard against this the script applies a `commit confirm 1` command. The `commit confirm 1` command applies the configuration changes but sets a rollback timer for 1 minute. If 1 minute passes and you have not issued a `commit` command the SRX will automatically rollback to the previous configuration. 
+
+
+* The script also checks to ensure that there are no pending configuration changes before applying it's own configuration changes. 
+
+
+* It also locks the configuration for a `configure exclusive`. This keeps users from applying changes at the same time the script is applying changes.
+
+
+* The script also issues a `commit check` command before attempting to commit any changes. The `commit check` command checks the candidate configuration for any obvious syntax errors (it does not safeguard against stupid, yet valid, configuration changes).  
+
+
+* If the script encounters any major errors it writes the error to the error file and moves on to the next IP address in the State list. 
+
+
+
+
---
+++ junos-pyez/commit-scripts/junos-set-config-change.py
@@ -0,0 +1,314 @@
+#Mike_Wazowski
+'''
+  \`"-.
+   )  _`-.
+  ,  : `. \
+  : _   '  \
+  ; *` _.   `--._
+  `-.-'          `-.
+    |       `       `.
+    :.       .        \
+    | \  .   :   .-'   .
+    :  )-.;  ;  /      :
+    :  ;  | :  :       ;-.
+    ; /   : |`-:     _ `- )
+ ,-' /  ,-' ; .-`- .' `--'
+ `--'   `---' `---' 
+  _                    _ _            
+ | |__   __ _ ___  ___| (_)_ __   ___ 
+ | '_ \ / _` / __|/ _ \ | | '_ \ / _ \
+ | |_) | (_| \__ \  __/ | | | | |  __/
+ |_.__/ \__,_|___/\___|_|_|_| |_|\___|                                      
+'''
+
+#This is a list of script specific modules that we are importing
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config 
+from jnpr.junos.exception import *
+from jnpr.junos.facts import *
+from lxml import etree
+from termcolor import *
+import colorama
+import datetime
+import getpass
+import pyping
+import sys
+import os
+
+# This points to the location of an IP list
+location = raw_input("""
+
+Select location:""")
+
+
+# Used to bring color to the sad/dead console of a windows machine
+colorama.init()
+
+bold_green = lambda x: cprint(x, 'green', attrs=['bold'])
+green = lambda x: cprint(x, 'green')
+yellow = lambda x: cprint(x, 'yellow', attrs=['bold'])
+red = lambda x: cprint(x, 'red', attrs=['bold'], file=sys.stderr)
+
+#The username is specified here and you will be prompted for a password.
+#getpass.getuser
+username = raw_input('Username:')
+password = getpass.getpass("Password:")
+
+fail_log = 'junos-config-change-error-report-%s.txt' % location
+
+#This is the 'out-file' that the script writes results to
+f = open('junos-config-change-results-%s.txt' % location, 'w')
+
+fail = open(fail_log, 'a')
+
+#The main function 'update_config' is defined below
+def update_config(host):
+
+	def date_time():
+		date = str(datetime.datetime.now())
+		return date
+
+	# This function has been known to cause timeout errors and is probably not the best way to get the location
+	def snmp_location():
+		data = dev.rpc.get_config()
+		location = data.xpath('//location')
+		locationstring = etree.tostring(location[0])
+		locationstrip = locationstring.replace("<location>", "").replace("</location>\n", "")
+		return locationstrip
+
+	model = dev.facts['model']
+
+	f.write("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" + '\n')
+	f.write("Working on: " + host + '\n')
+	f.write(snmp_location() + '\n')
+	print "Working on:", host
+	print snmp_location()
+	f.write(date_time() + '\n')
+	print datetime.datetime.now()
+	print "Model:", model 
+	f.write("Model: " + model + '\n')
+
+	#This binds Config to dev
+	cu = Config(dev)
+
+	hostname = dev.facts['hostname']
+
+	#This is where the ping function is defined. This function is used later in the script to test connectivity to the SRX
+	def pingtest(host):
+		r = pyping.ping(host.strip())
+		if r.ret_code is 0:
+			return True
+		else:
+			return False
+	
+    #This is where the Juniper 'set' commands are defined. These are used to apply the configuration changes
+	set_commands = """
+	set system scripts op file sit.slax description "null"
+	"""
+
+	f.write("Locking the configuration..." + '\n')
+	print "Locking the configuration..."
+    
+	#This command locks the configuration for a 'configure exclusive'
+	try:
+		cu.lock()
+		f.write("Configuration locked" + '\n')
+		green("Configuration locked")
+	except (KeyboardInterrupt, SystemExit):
+		raise
+	except LockError as err:
+		f.write("Error: Unable to lock configuration: {0} ".format(err) + '\n')
+		red("Error: Unable to lock configuration...")
+
+	f.write("Checking for uncommitted configuration changes..." + '\n')
+	print "Checking for uncommitted configuration changes..."
+
+	#This command is the equivilant of 'show | compare'
+	if cu.diff() == None:
+		f.write("There are no uncommitted changes" + '\n')
+		green("There are no uncommitted changes")
+	else:
+		f.write("There are uncommitted changes...rolling back..." + '\n')
+		yellow("There are uncommmitted changes...rolling back...")
+		try:
+			cu.rollback(rb_id=0) #This command issues a rollback. 'rb_id=' is the rollabck number: 0 - 5
+			f.write("Configuration has been rolled back to 0" + '\n')
+			f.write("Locking configuration..." + '\n')
+			green("Configuration has been rolled back!")
+			try:
+				f.write("Double checking 'show | compare'" + '\n')
+				print "Double checking 'show | compare'"
+				if cu.diff() == None:
+					try:
+						print "Locking configuration..."
+						cu.lock() #This command is used to lock the configuration in the event that the first
+						f.write("Configuration locked" + '\n') #configuiration lock failed due to uncommitted configuration changes
+						print "Configuration locked" 
+					except (KeyboardInterrupt, SystemExit):
+						raise
+					except LockError as err:
+						f.write("Unable to lock configuration: {0} ".format(err) + '\n')
+						red("Error: Unable to lock configuration!")
+						f.write("Exiting this device immediately!" + '\n')
+						print "Exiting this device immediately!"
+						return
+				else:
+					fail.write("There were problems removing the pending configuration changes on " + hostname + '\n')
+					f.write("There were problems removing the pending configuration changes" + '\n')
+					f.write("Exiting now..." + '\n')
+					print "There were problems removing the pending configuration changes"
+					print "Exiting now"
+					return
+
+			except (KeyboardInterrupt, SystemExit):
+				raise
+			except:
+				fail.write("There were probelms running 'show | compare' on " + hostname + '\n')
+				f.write("There were errors running 'show | compare' " + '\n')
+				f.write("Exiting device immediately" + '\n')
+				print "There were errors running 'show | compare' "
+				print "Exiting now"
+				return
+
+		except (KeyboardInterrupt, SystemExit):
+			raise
+		except:
+			fail.write("Failed to rollback on " + hostname + '\n')
+			f.write("Failed to rollback..." + '\n')
+			f.write("Exiting this device" + '\n')
+			print "Failed to rollback..."
+			print "Exiting this device"
+			dev.close()
+			return
+
+	f.write("Loading the configuration changes..." + '\n')
+	print "Loading the configuration changes..."
+
+	#This command loads the configuration changes. The 'merge=False' parameter means it will overwrite existing configurations
+	try:
+		cu.load(set_commands,format='set',merge=False)
+	except (KeyboardInterrupt, SystemExit):
+		raise
+	except ValueError as err:  
+		fail.write("Failed to load configuration changes {0} ".format(err) + '\n')                         
+		f.write("Loading the configuration changes failed: {0} ".format(err) + '\n') 
+		f.write("Exiting the device" + '\n')                       
+		red(err.message) 
+		return
+
+	try:
+		f.write("Checking commit..." + '\n')
+		print "Checking commit..."
+		cu.commit_check()
+		f.write("Commit check passed" + '\n')
+		print "Commit check passed with 0 errors"
+	except CommitError as err:
+		fail.write("Commit check did NOT pass {0} ".format(err) + '\n')
+		f.write("Commit check did NOT pass! {0} ".format(err) + '\n')
+		print "Commit check did NOT pass! Check log file for more"
+		return
+	except RpcError as err:
+		fail.write("Commit check did NOT pass {0} ".format(err) + '\n')
+		f.write("Commit check did NOT pass! {0} ".format(err) + '\n')
+		print "Commit check did NOT pass! Check log file for more"
+		return
+
+	f.write("Committing the configuration..." + '\n')
+	print "Committing the configuration changes..."
+
+	#This command commits the config with a comment and 1 minute to confirm
+	try:
+		cu.commit(confirm=1)
+		f.write("Commit confirm 1 successful!" + '\n')
+		bold_green("Commit confirm 1 successful!"),
+		print "Verifying connectivity..."
+	except (KeyboardInterrupt, SystemExit):
+		raise
+	except CommitError as err:
+		fail.write("Commit failed or broke connectivity: {0} ".format(err) + '\n')
+		f.write("Commit failed or broke conectivity: {0} ".format(err) + '\n')
+		f.write("Pinging to verify..." + '\n')
+		red("Commit failed or broke connectivity")
+		print "Pinging to verify..."
+	except RpcTimeoutError as err:
+		fail.write("Commit failed or broke connectivity: {0} ".format(err) + '\n')
+		f.write("Commit failed or broke conectivity: {0} ".format(err) + '\n')
+		f.write("Pinging to verify..." + '\n')
+		red("Commit failed or broke connectivity")
+		print "Pinging to verify..."
+
+	#This command is used to ping the host to see if applying the configuration broke the connectivity
+	if pingtest(host) is True:  
+		f.write(host.strip() + " looks up from here" + '\n')
+		f.write("Confirming the configuration..." + '\n')
+		print host.strip() ,"Looks UP from here"
+		print "Confirming the configuration..."
+		try:
+			cu.commit(comment="junos-rootpswd-change") #If the ping succeeds it issues a 'commit confirm'
+			f.write("Configuration was confirmed!" + '\n')
+			f.write("Unlocking the configuration..." + '\n')
+			bold_green("Configuration was confirmed!") 
+			print "Unlocking the configuration..."                                          
+			try:
+				cu.unlock() #This command unlocks the configuration
+			except (KeyboardInterrupt, SystemExit):
+				raise
+			except UnlockError as err:
+				f.write("Unlocking the configuration failed: {0} ".format(err) + '\n')
+				red("Unlocking the configuration failed")
+
+			f.write("Closing connection to " + hostname + '\n')
+			print "Closing the connection to", hostname
+			try:
+				dev.close() #This command closes the device connection 
+			except (KeyboardInterrupt, SystemExit):
+				raise
+			except:
+				f.write("Failed to close " + hostname + '\n')
+				print "Failed to close", hostname
+
+		except (KeyboardInterrupt, SystemExit):
+			raise
+		except CommitError as err:
+			fail.write("Commit failed: {0} ".format(err) + '\n')
+			f.write("Failed to commit changes: {0} ".format(err) + '\n')
+			print "Failed to commit the changes"
+		except RpcTimeoutError as err:
+			print "RPC timeout error!"
+			f.write("RPC timeout error!" + '\n')
+			fail.write("RPC timeout error!" + '\n')
+	else:  #This detects ping failure. If the ping fails, the script will exit the current host without confirming
+		f.write(host.strip() + " looks DOWN from here..." + '\n') #the commit and the configuration will rollback in 1 minute
+		fail.write(host + "looks down from here" + '\n')
+		f.write("Moving on to the next host..." + '\n')        
+		print host ,"looks down from here..."
+		print "Moving on to the next host..."
+
+	f.write("Completed: " + hostname + '\n' )
+	f.write("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-" + '\n')
+	f.write('\n')
+	print "Completed:", hostname 
+	print ""
+
+#This is used to loop through every host entry in the list specified
+with open('C:\\ip_lists/%s-list.txt' % location) as infile:  
+	for host in infile:
+
+		#This is used to test connectivity to a host before attempting to connect
+		dev = Device(host=host.strip(), user=username, password=password) #change pass function
+		try:
+			dev.open()                                                     
+		except (KeyboardInterrupt, SystemExit):
+			raise
+		except ConnectError as err:
+			fail.write("Error: Cannot connect to device {0} ".format(err) + '\n')                                           
+			f.write("Error: Cannot connect to device: {0} ".format(err) + '\n')
+			yellow("Error: Cannot connect to device: {0} ".format(err))
+			continue
+		update_config(host)
+
+f.close()
+fail.close()
+
+if os.path.getsize(fail_log) == 0:
+	os.remove(fail_log)
